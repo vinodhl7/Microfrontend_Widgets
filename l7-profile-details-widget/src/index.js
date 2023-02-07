3c4d003a0a61296e17895f24c734ec891316e352
simon <simon@litmus7.com>
2023-02-07 12:40:45 +0530

    styles added

--- l7-profile-details-widget/src/index.js
+++ l7-profile-details-widget/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
+import './tailwind.css'
 import {retrieveProfile} from './service/profile-widget-api'
 import { useState, useEffect } from "react";
 
@@ -24,30 +25,41 @@ export const ProfileDetailsWidget = ({widgetId, customerId, env, context  }) => 
 
   
 
-  return <div className={styles.test}>Profile Details
-     <div>
-        &nbsp; 
-    </div>
-    
-    <div>
-        First Name: {customer?.firstName} 
-    </div>
+  return (
+    <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <div className='flex flex-col items-center pb-10'>
+        <img
+          class='w-24 h-24 mb-3 rounded-full shadow-lg'
+          src={customer.avatar}
+          alt='avatar'
+        />
+        <h5 class='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
+          {customer.lastName}, {customer.firstName}
+        </h5>
+        <div class='flex mt-4 space-x-3 md:mt-6'></div>
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          Last login: {customer.updatedAt}
+        </div>
 
-    <div>
-        Last Name: {customer?.lastName} 
-    </div>
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          Phone Number: {customer.phoneNumber}
+        </div>
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          email: {customer.email}
+        </div>
 
-    <div>
-        Email: {customer?.email} 
-    </div>
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          City: {customer.city}
+        </div>
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          State: {customer.state}
+        </div>
 
-    <div>
-        City: {customer?.city} 
+        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+          Created at: {customer.createdAt}
+        </div>
+      </div>
     </div>
-    <div>
-        Phone: {customer?.phoneNumber} 
-    </div>
-   
-    </div>
+  )
 }
 
--- l7-profile-list-widget/src/index.js
+++ l7-profile-list-widget/src/index.js
@@ -1,37 +1,72 @@
 import React from 'react'
 import styles from './styles.module.css'
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
+import './tailwind.css'
 
-
-export const ProfileListWidget = ({widgetId, env, context  }) => {
-  
-  const [customers, setCustomers] = useState([]);
+export const ProfileListWidget = ({ widgetId, env, context }) => {
+  const [customers, setCustomers] = useState([])
 
   useEffect(() => {
     fetch('https://6326b210ba4a9c4753299365.mockapi.io/litmus7/users/')
-    .then((response) => response.json())
-    .then((data) => {
-       console.log('profile_api_data' + data);
-       setCustomers(data);
-    })
-    .catch((err) => {
-       console.log(err.message);
-    });
-    
-    
-  }, []);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('profile_api_data' + data)
+        setCustomers(data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
 
-  
-
-  return <div className={styles.test}>Profile List
-    <ul>
-    {customers?.map( (customer) =>  {
-      return (<li>
-        <a href={'/customer/customer-detail/'+ customer.id}> {customer.firstName + ' ' + customer.lastName} </a>
-      </li>);
-    })  
-    }
-    </ul>
-   
+  return (
+    <div className='relative overflow-x-auto'>
+      <table className='w-full text-sm text-left text-gray-500 '>
+        <thead className='text-xs text-gray-700 uppercase bg-gray-50 '>
+          <tr>
+            <th scope='col' className='px-6 py-3'>
+              email
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              profile id
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              name
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              billing phone number
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              city
+            </th>
+            <th scope='col' className='px-6 py-3'>
+              state
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers?.map((customer) => (
+            <tr className='bg-white border-b '>
+              <th
+                scope='row'
+                className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '
+              >
+                {customer.email}
+              </th>
+              <td className='px-6 py-4'>{customer.id}</td>
+              <td className='px-6 py-4'>
+                {
+                  <a href={'/customer/customer-detail/' + customer.id}>
+                    {customer.firstName + ' ' + customer.lastName}{' '}
+                  </a>
+                }
+              </td>
+              <td className='px-6 py-4'>{customer.phoneNumber}</td>
+              <td className='px-6 py-4'>{customer.city}</td>
+              <td className='px-6 py-4'>{customer.state}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+  )
 }

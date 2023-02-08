8972c0700ba15fc1b3f93cd5191943473a8339a5
simon <simon@litmus7.com>
2023-02-08 12:47:51 +0530

    order details styles updated

--- l7-order-detail-widget/src/index.js
+++ l7-order-detail-widget/src/index.js
@@ -2,12 +2,7 @@ import React from 'react'
 import './tailwind.css'
 import { useState, useEffect } from 'react'
 
-export const OrderDetailsWidget = ({
-  widgetId,
-  orderId,
-  env,
-  context
-}) => {
+export const OrderDetailsWidget = ({ widgetId, orderId, env, context }) => {
   const [order, setOrder] = useState({})
 
   useEffect(() => {
@@ -27,32 +22,33 @@ export const OrderDetailsWidget = ({
   return (
     <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
       <div className='flex flex-col items-center pb-10'>
-        <h5 class='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
+        <h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
           Order id : {order.orderId}
         </h5>
-        <h5 class='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
-          Price: {order.totalPrice}
+        <h5 className='mb-1 text-xl font-medium text-gray-900 dark:text-white'>
+          Price: ${order.totalPrice}
         </h5>
-        <div class='flex mt-4 space-x-3 md:mt-6'></div>
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm  text-left'>
           Customer id : {order.customerId}
         </div>
 
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm  '>
           Name : {order.lastName},{order.firstName}
+        </div>
+        <div className='inline-flex items-start px-2 py-1 text-sm text-left'>
           Guest : {order.guestOrder ? 'yes' : 'no'}
         </div>
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm text-left'>
           Created at: {new Date(order.createdAt).toLocaleString()}
         </div>
 
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm  text-left'>
           Date submitted:{new Date(order.dateSubmitted).toLocaleString()}
         </div>
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm text-left'>
           Date shipped: {new Date(order.dateShipped).toLocaleString()}
         </div>
-        <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
+        <div className='inline-flex items-start px-2 py-1 text-sm  text-left'>
           Phone Number: {order.phoneNumber}
         </div>
       </div>

c845219698d27b3614c9de469033c9f65610ccf1
simon <simon@litmus7.com>
2023-02-08 13:06:07 +0530

    profile details ui updated

--- l7-profile-details-widget/src/index.js
+++ l7-profile-details-widget/src/index.js
@@ -39,8 +39,8 @@ export const ProfileDetailsWidget = ({
   }
 
   return (
-    <div className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-      <div className='flex flex-col items-center pb-10'>
+    <div className='w-full max-w-sm p-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+      <div className='flex flex-col items-center'>
         <img
           class='w-24 h-24 mb-3 rounded-full shadow-lg'
           src={customer.avatar}
@@ -71,15 +71,15 @@ export const ProfileDetailsWidget = ({
         <div className='inline-flex items-start px-2 py-1 text-sm font-medium text-left'>
           Created at: {customer.createdAt}
         </div>
+        <button
+          className='py-2.5 px-5 mr-2 mb-2 mt-5 text-sm font-medium text-gray-900 focus:outline-none bg-blue-200 rounded-lg border border-gray-200 hover:bg-blue-300 hover:text-gray-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
+          onClick={() => {
+            setContextHandler(customer)
+          }}
+        >
+          Select customer!!
+        </button>
       </div>
-      <button
-        className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-        onClick={() => {
-          setContextHandler(customer)
-        }}
-      >
-        Select customer!!
-      </button>
     </div>
   )
 }
